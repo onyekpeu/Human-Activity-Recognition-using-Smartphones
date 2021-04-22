69cb3462be2ebc29b765a88cf58f75f9da2a6254
Uche Onyekpe <46845941+onyekpeu@users.noreply.github.com>
2021-04-22 22:12:48 +0100

    Update Main file (with fourier transforms).py

--- Main file (with fourier transforms).py
+++ Main file (with fourier transforms).py
@@ -9,11 +9,9 @@ Created on Fri Oct 30 17:52:52 2020
 import os
 import numpy as np
 import os.path
-#import pandas as pd
-#from sklearn.metrics import accuracy_score,roc_auc_score
+
 import tensorflow.keras
-#from keras.layers import Dense, Flatten
-#from keras.layers import Conv2D, MaxPooling2D
+
 from tensorflow.keras.models import Sequential
 
 from tensorflow.keras.layers import Dense
@@ -21,9 +19,7 @@ from tensorflow.keras.layers import LSTM, GRU, SimpleRNN, Bidirectional
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.callbacks import EarlyStopping, History, LearningRateScheduler
 from tensorflow.keras import regularizers
-#from keras.layers import Recurrent_Dropout
-# from tensorflow.keras.utils.vis_utils import plot_model
-#from tensorflow.keras import backend as K
+
 from tensorflow.keras import optimizers 
 import time
 import matplotlib.pyplot as plt
@@ -76,15 +72,12 @@ from scipy.fftpack import fft
 def get_fft_values(y_values, N):
     fft_values_ = fft(y_values)
     fft_values = np.abs(fft_values_[0:N//2])
-#    fft_values = 2.0/N * np.abs(fft_values_[0:N//2])
     return fft_values
 
 
 def to_fourier(data,N,seq,input_dim):
     Data = np.zeros((data.shape[0], seq, input_dim))
     for ii in range(len(data)):
-    #        if ii % 1000 == 0:
-    ##            print(ii)
         for jj in range(0,input_dim):
             signal = data[ii, :, jj]
             coeff = get_fft_values(signal, N)
@@ -96,28 +89,8 @@ def to_fourier(data,N,seq,input_dim):
 y_train=list(train_labels_ucihar)
 y_test=list(test_labels_ucihar)
 
-#batch size
-#32-96.97/96.42
-#64-97.1/96.97
-#96-96.69/96.83
-#128-96.76/96.18
-#256-96.87/96.33
-#320-97.04/96.08
-#416-96.76/96.49
-#512- 96.52/95.91
-#720-96.35\96.21
-#1024- 96.22/95.47
-#1280- 95.54/95.71
-#weights
-
-#8-95.53
-#16-93.08
-#32-96.45
-#64-96.73
-#96-97.31
-#128-97.68
-#256-97.24
-#512-97.21
+y_train1 = tensorflow.keras.utils.to_categorical(np.array(y_train)-1, num_classes)
+y_test1 = tensorflow.keras.utils.to_categorical(np.array(y_test)-1, num_classes)
 
 
 # par=[1,2,1]
@@ -125,53 +98,46 @@ y_test=list(test_labels_ucihar)
 
 par=np.arange(1, 2,1)#14
 opt_runs=np.zeros((len(par),1))
-opt_runs1=np.zeros((len(par),1))
 for opt_ in range(len(par)):
     opt=par[opt_] 
-    seq=4#int(opt)#0#number of timesteps
     N=24
     input_shape=9
     num_classes = 6
     batch_size = 16#64
-    num_classes = 7
     epochs = 100
-    h2=100#32
+    hidden_dim=32
     dropout=0.25
     learning_rate=0.0002502  
-    nor=1
+    number_of_training_runs=1
     seq_dim=12
 
     
     x_train1=to_fourier(train_signals_ucihar,N,seq_dim,input_shape) 
     x_test1=to_fourier(test_signals_ucihar,N,seq_dim,input_shape) 
     
-    n_runs=np.zeros((int(nor),1))    
-    n_runs1=np.zeros((int(nor),1)) 
-    for nfr in range(nor):
+    n_runs=np.zeros((int(number_of_training_runs),1))    
+    for nfr in range(number_of_training_runs):
         print('full training run: '+ str(nfr))
         print('optimisation run: '+ str(opt))    
      
         history = History()
-     
-        y_train1 = tensorflow.keras.utils.to_categorical(np.array(y_train)-1, num_classes)
-        y_test1 = tensorflow.keras.utils.to_categorical(np.array(y_test)-1, num_classes)
-        
+             
         start=time.time()
         regressor = Sequential()
-        regressor.add(GRU(units =h2,input_shape = (x_train1.shape[1], x_train1.shape[2]), activation="tanh", recurrent_activation="sigmoid", use_bias=True, kernel_initializer="glorot_uniform", recurrent_initializer="orthogonal", recurrent_dropout=dropout, return_sequences = False))
+        regressor.add(GRU(units =hidden_dim,input_shape = (x_train1.shape[1], x_train1.shape[2]), activation="tanh", recurrent_activation="sigmoid", use_bias=True, kernel_initializer="glorot_uniform", recurrent_initializer="orthogonal", recurrent_dropout=dropout, return_sequences = False))
 #        regressor.add(Dropout(dropout))
 #        regressor.add(GRU(units = h2, activation="tanh", recurrent_activation="sigmoid", use_bias=True, kernel_initializer="glorot_uniform", recurrent_initializer="orthogonal", recurrent_dropout=dropout, return_sequences = True))
 #        regressor.add(Dropout(dropout))
 #        regressor.add(GRU(units = h2, activation="tanh", recurrent_activation="sigmoid", use_bias=True, kernel_initializer="glorot_uniform", recurrent_initializer="orthogonal", recurrent_dropout=dropout, return_sequences = False))
 #            
         regressor.add(Dense(units = num_classes, activation='sigmoid'))
-        adamax=optimizers.Adam(lr=learning_rate)#, beta_1=0.9, beta_2=0.99)     
+        adamax=optimizers.Adamax(lr=learning_rate)#, beta_1=0.9, beta_2=0.99)     
         regressor.compile(loss=tensorflow.keras.losses.categorical_crossentropy,
                       optimizer=adamax,
                       metrics=['accuracy'])   
         print(regressor.summary())
         
-        regressor.fit(x_train1, y_train1, batch_size=batch_size, epochs=epochs, verbose=1,validation_data=(x_test1, y_test1), callbacks=[history])
+        regressor.fit(x_train1, y_train1, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[history])
         plt.plot(history.history['loss'], label='train')
         
         plt.show()
